9adae17399b45bcd19d1ec56eb8d6a43ee041c47
bbrodsky <bbrodsky@gmail.com>
2017-01-14 13:46:37 -0500

    added uid

--- app/components/friendlyChat.js
+++ app/components/friendlyChat.js
@@ -17,11 +17,9 @@
 
 // Initializes FriendlyChat.
 export function FriendlyChat() {
-  // this.checkSetup();
+  this.checkSetup();
 
   // Shortcuts to DOM Elements.
-  this.messageContainer = document.getElementById('messages-card-container');
-  this.messageCard = document.getElementById('messages-card');
   this.messageList = document.getElementById('messages');
   this.messageForm = document.getElementById('message-form');
   this.messageInput = document.getElementById('message');
@@ -76,8 +74,8 @@ FriendlyChat.prototype.loadMessages = function() {
     var val = data.val();
     this.displayMessage(data.key, val.name, val.text, val.photoUrl, val.imageUrl);
   }.bind(this);
-  this.messagesRef.limitToLast(6).on('child_added', setMessage);
-  this.messagesRef.limitToLast(6).on('child_changed', setMessage);
+  this.messagesRef.limitToLast(12).on('child_added', setMessage);
+  this.messagesRef.limitToLast(12).on('child_changed', setMessage);
 };
 
 // Saves a new message on the Firebase DB.
@@ -88,6 +86,7 @@ FriendlyChat.prototype.saveMessage = function(e) {
     var currentUser = this.auth.currentUser;
     // Add a new message entry to the Firebase Database.
     this.messagesRef.push({
+      uid: currentUser.uid,
       name: currentUser.displayName,
       text: this.messageInput.value,
       photoUrl: currentUser.photoURL || '/images/profile_placeholder.png'
@@ -236,7 +235,6 @@ FriendlyChat.LOADING_IMAGE_URL = 'https://www.google.com/images/spin-32.gif';
 
 // Displays a Message in the UI.
 FriendlyChat.prototype.displayMessage = function(key, name, text, picUrl, imageUri) {
-  // console.log("messages", arguments)
   var div = document.getElementById(key);
   // If an element for that message does not exists yet we create it.
   if (!div) {
@@ -244,10 +242,7 @@ FriendlyChat.prototype.displayMessage = function(key, name, text, picUrl, imageU
     container.innerHTML = FriendlyChat.MESSAGE_TEMPLATE;
     div = container.firstChild;
     div.setAttribute('id', key);
-    // styling for comments
-    div.className += " mdl-card mdl-shadow--2dp mdl-cell mdl-cell--12-col mdl-cell--4-col-tablet mdl-cell--4-col-desktop";
-    div.firstChild.className += " mdl-card__supporting-text mdl-color-text--grey-600"
-    this.messageContainer.appendChild(div);
+    this.messageList.appendChild(div);
   }
   if (picUrl) {
     div.querySelector('.pic').style.backgroundImage = 'url(' + picUrl + ')';
@@ -298,6 +293,6 @@ FriendlyChat.prototype.checkSetup = function() {
   }
 };
 
-// window.onload = function() {
-//   window.friendlyChat = new FriendlyChat();
-// };
+window.onload = function() {
+  window.friendlyChat = new FriendlyChat();
+};

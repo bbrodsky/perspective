87e1bcb270e194898b37381749c30f15f420a262
Shane Jarman <sjarman91@gmail.com>
2017-01-14 17:47:10 -0500

    layout init

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -12,15 +12,37 @@ class Home extends Component {
     const { user } = this.props
 
     return (
-    <div>
-    {user && user.displayName ?
-    <div>
-      <p>you are logged in as: {user.displayName}</p>
-      <img className="swag" src={user.photoURL} />
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6">
+          <div className="vbox"></div>
+        </div>
+        <div className="col-md-6">
+          <div className="vbox"></div>
+        </div>
+      </div>
+        {user && user.displayName ?
+        <div className="row">
+          <div className="col-md-6">
+            <p>you are logged in as: {user.displayName}</p>
+          </div>
+          <div className="col-md-6">
+            <img className="swag" src={user.photoURL} />
+          </div>
+        </div>
+        :
+        <p>You are not logged in</p>}
+      <div className="row">
+        <h1> video section</h1>
+      </div>
+      <div className="row">
+        <h1> flying links section</h1>
+      </div>
+      <div className="row">
+        <h1> comments section</h1>
+      </div>
     </div>
-    :
-    <p>You are not logged in</p>}
-    </div>
+
     )
   }
 }

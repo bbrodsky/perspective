0ffedfeefd294eac05604a26f2a250dd26d8ac4a
mithunder <mselvaratnam@gmail.com>
2017-01-14 20:23:42 -0500

    dont work

--- app/components/Chat.jsx
+++ app/components/Chat.jsx
@@ -3,25 +3,57 @@ import { connect } from 'react-redux';
 import CommentForm from './CommentForm.jsx';
 import TopComments from './TopComments.jsx';
 import Comment from './Comment.jsx';
+import FlipMove from 'react-flip-move';
 
 class Chat extends Component {
   constructor(props) {
     super(props);
+    this.renderRecentComments = this.renderRecentComments.bind(this);
+    this.renderTopComments = this.renderTopComments.bind(this);
   }
 
-  render() {
-    const { messages, firebase } = this.props;
-
+  renderRecentComments(){
+    const { messages, firebase} = this.props;
     var commentArray = Object.keys(messages).map(function (key) {
       let msg = messages[key];
       msg.id = key
       return msg;
     });
 
-    let duplicate = Object.assign([], commentArray)
-    let topRated = duplicate.sort((a, b) => b.score - a.score).slice(0,6)
-
     commentArray = commentArray.slice(commentArray.length-6).reverse();
+
+    if (!commentArray) return;
+    return commentArray.map((comment, index) => (
+        <Comment key={index} firebase={firebase} message={comment} userName={comment.name} />
+      )
+    )
+  }
+
+  renderTopComments(){
+    const { messages, firebase } = this.props;
+    var topCommentArray = Object.keys(messages).map(function (key) {
+      let msg = messages[key];
+      msg.id = key
+      return msg;
+    });
+    let topRated = topCommentArray.sort((a, b) => b.score - a.score).slice(0,6);
+
+    return <TopComments comments={topRated} firebase={firebase}/>
+  }
+
+  render() {
+    const { firebase } = this.props;
+    //
+    // var commentArray = Object.keys(messages).map(function (key) {
+    //   let msg = messages[key];
+    //   msg.id = key
+    //   return msg;
+    // });
+    //
+    // let duplicate = Object.assign([], commentArray)
+    // let topRated = duplicate.sort((a, b) => b.score - a.score).slice(0,6)
+    //
+    // commentArray = commentArray.slice(commentArray.length-6).reverse();
 
     return (
     <div className="row">
@@ -31,15 +63,21 @@ class Chat extends Component {
         </div>
         <div className="row">
           <div className="col-md-6">
-            <TopComments comments={topRated} firebase={firebase}/>
+            {/* <TopComments comments={topRated} firebase={firebase}/> */}
+            <FlipMove easing="ease" style={{width: '100%'}}>
+              { this.renderTopComments() }
+            </FlipMove>
           </div>
           <div className="col-md-6">
           <h4>Recent Comments</h4>
-          {commentArray.length && commentArray.map((comment, index) => {
+          <FlipMove easing="ease" style={{width: '100%'}}>
+            { this.renderRecentComments() }
+          </FlipMove>
+          {/* {commentArray.length && commentArray.map((comment, index) => {
             return (
               <Comment key={index} firebase={firebase} message={comment} userName={comment.name} />
             )
-          })}
+          })} */}
           </div>
         </div>
       </div>

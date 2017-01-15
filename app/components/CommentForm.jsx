a2f0535cddbe9117bc5adb8d7126fbc813779a87
bbrodsky <bbrodsky@gmail.com>
2017-01-15 11:50:45 -0500

    added issue submit skeleton

--- app/components/CommentForm.jsx
+++ app/components/CommentForm.jsx
@@ -23,34 +23,54 @@ export default class CommentForm extends Component {
 
   render() {
     return (
-      <div id="messages-form" className="col-md-12 mdl-card mdl-shadow--2dp">
+    <div className="row col-md-12">
+      <div id="messages-form" className="col-md-6 mdl-card mdl-shadow--2dp">
         <div className="mdl-card__supporting-text mdl-color-text--grey-600">
+          Comment on the issue at hand!
           <form id="message-form" action="#" onSubmit={(e) => this.submitComment(e)}>
             <div className="row">
               <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label
-                col-md-12">
+                col-md-6">
+
                 <input className="mdl-textfield__input" type="text" id="message" />
-                <label className="mdl-textfield__label" htmlFor="message">Message...</label>
+                <label className="mdl-textfield__label" htmlFor="message">I like how you phrased that...</label>
               </div>
             </div>
             <div className="row">
-            <div className="col-md-6">
-              <button id="submit" form="message-form" className="mdl-button submit-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">
-                Send
-              </button>
-            </div>
-            {/*<div className="col-md-6">
-            <input id="mediaCapture" type="file" accept="image/*,capture=camera" />
-             <button id="submitImage" title="Add an image" className="mdl-button picture-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-color--amber-400 mdl-color-text--white">
-              <i className="material-icons">image</i>
-            </button>
-            </div>*/}
+              <div className="col-md-3">
+                <button id="submit" form="message-form" className="mdl-button submit-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">
+                  Send
+                </button>
+              </div>
             </div>
           </form>
           <form id="image-form" action="#">
           </form>
         </div>
       </div>
+      <div id="messages-form" className="col-md-6 mdl-card mdl-shadow--2dp">
+        <div className="mdl-card__supporting-text mdl-color-text--grey-600">
+          To participate, submit an interest statement
+          <form>
+            <div className="row">
+              <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label
+                col-md-6">
+
+                <input className="mdl-textfield__input" type="text" id="message" />
+                <label className="mdl-textfield__label" htmlFor="message">I am intersted in....</label>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-3">
+                <button id="submit" form="message-form" className="mdl-button submit-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">
+                  Send
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
     )
   }
 }
